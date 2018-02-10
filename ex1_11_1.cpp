fd5559728c35b48dcfeccac2af9a173f75bf8261
samzhuwj <samzhuwj@gmail.com>
2018-02-10 19:10:12 +0800

    print each number in the range specified by two integers

---
+++ ex1_11_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "please input two integers:\n";
+    int small = 0, big = 0;
+    std::cin >> small >> big;
+
+    if (small > big)
+    {
+        int tmp = small;
+        small = big;
+        big = tmp;
+    }
+
+    for (int i = small; i != big; ++i)
+        std::cout << i << std::endl;
+
+    return 0;
+}
