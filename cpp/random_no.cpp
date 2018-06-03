86556b6e4fa460a2f909c71f6be12fab19fe5107
samzhuwj <samzhuwj@gmail.com>
2018-06-03 15:54:41 +0800

    numbers and arrays

---
+++ cpp/random_no.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main () {
+  int i,j;
+
+  // set the seed
+  srand( (unsigned)time( NULL ) );
+
+  for( i = 0; i < 10; i++ ) {
+    // generate actual random number
+    j = rand();
+    cout <<" Random Number : " << j << endl;
+  }
+
+  return 0;
+}
