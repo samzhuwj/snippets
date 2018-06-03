05d4ee92d0d7882838622d56adc392e7ecda401d
samzhuwj <samzhuwj@gmail.com>
2018-06-03 13:51:45 +0800

    storage class

---
+++ cpp/const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const int  LENGTH = 17;
+  const int  WIDTH  = 9;
+  const char NEWLINE = '\n';
+  int area;
+
+  area = LENGTH * WIDTH;
+  cout << area;
+  cout << NEWLINE;
+  return 0;
+}
---
+++ cpp/define.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define LENGTH 17
+#define WIDTH  9
+#define NEWLINE '\n'
+
+int main() {
+  int area;
+
+  area = LENGTH * WIDTH;
+  cout << area;
+  cout << NEWLINE;
+  return 0;
+}
