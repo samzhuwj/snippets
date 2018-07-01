ee4b109984eae16043e7cab29a0b9555d370ea4c
samzhuwj <samzhuwj@gmail.com>
2018-07-01 15:18:48 +0800

    template classes

--- u2cvt/79.cpp
+++ u2cvt/79.cpp
@@ -1,23 +1,6 @@
 // use template function
 #include <iostream>
 
-template <typename T> void Times2(T val);
-template <typename T> T Add(T val, T val2);
-template <typename T> T Max(T val, T val2);
-
-int main()
-{
-  Times2(5);
-  Times2(5.3);
-  std::cout << "5 + 4 = " << Add(5,4) << "\n";
-  std::cout << "5.5 + 4.6 = " << Add(5.5,4.6) << "\n";
-  std::cout << "Max 4 or 8 = " << Max(4, 8) << "\n";
-  std::cout << "Max A or B = " << Max('A', 'B') << "\n";
-  std::cout << "Max Dog or Cat = " << Max("Dog", "Cat") << "\n";
-
-  return 0;
-}
-
 template <typename T> void Times2(T val)
 {
   std::cout << val << " * 2 = " << val * 2 << "\n";
@@ -31,4 +14,17 @@ template <typename T> T Add(T val, T val2)
 template <typename T> T Max(T val, T val2)
 {
   return (val < val2) ? val2 : val;
+}
+
+int main()
+{
+  Times2(5);
+  Times2(5.3);
+  std::cout << "5 + 4 = " << Add(5,4) << "\n";
+  std::cout << "5.5 + 4.6 = " << Add(5.5,4.6) << "\n";
+  std::cout << "Max 4 or 8 = " << Max(4, 8) << "\n";
+  std::cout << "Max A or B = " << Max('A', 'B') << "\n";
+  std::cout << "Max Dog or Cat = " << Max("Dog", "Cat") << "\n";
+
+  return 0;
 }
